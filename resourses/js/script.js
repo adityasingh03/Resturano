4f27d30e50e67427874effa03390a8b2f206cd32
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-06-13 11:14:46 +0530

    13/06

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -4,10 +4,15 @@
 document.querySelector(".sinin").addEventListener("click", function () {
   document.querySelector(".modalp").classList.remove("modalph");
   document.querySelector(".modalp").classList.add("modalpb");
-  console.log(1);
 });
 
-document.querySelector(".modalp").addEventListener("click", function () {
+document.querySelector(".overlay").addEventListener("click", function () {
+  document.querySelector(".modalp").classList.add("modalph");
+  document.querySelector(".modalp").classList.remove("modalpb");
+});
+
+document.querySelector(".overlay").addEventListener("keydown", function (e) {
+  console.log(e.key);
   document.querySelector(".modalp").classList.add("modalph");
   document.querySelector(".modalp").classList.remove("modalpb");
   console.log(1);
