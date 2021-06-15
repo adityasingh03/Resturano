365c0cbd3db47d0990be46f51231b6e8155e2a59
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-06-15 16:50:47 +0530

    15/06

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -3,6 +3,9 @@ const closebtn = document.querySelector(".containh3img");
 const signin = document.querySelector(".sinin");
 const overlay = document.querySelector(".overlay");
 
+const login = document.querySelector(".login");
+const register = document.querySelector(".register");
+const forgot = document.querySelector(".forgot");
 //functions
 const f1 = function (e) {
   e.preventDefault();
@@ -14,9 +17,50 @@ const f2 = function () {
   document.querySelector(".modalp").classList.add("modalph");
   document.querySelector(".modalp").classList.remove("modalpb");
 };
+
+const f3 = function () {
+  register.classList.add("activem");
+  document.querySelector(".registerc").classList.remove("hidden");
+  if (!login.classList.contains("hidden")) {
+    login.classList.remove("activem");
+    document.querySelector(".loginc").classList.add("hidden");
+  }
+  if (!forgot.classList.contains("hidden")) {
+    forgot.classList.remove("activem");
+    document.querySelector(".forgotc").classList.add("hidden");
+  }
+};
+const f4 = function () {
+  login.classList.add("activem");
+  document.querySelector(".loginc").classList.remove("hidden");
+  if (!register.classList.contains("hidden")) {
+    register.classList.remove("activem");
+    document.querySelector(".registerc").classList.add("hidden");
+  }
+  if (!forgot.classList.contains("hidden")) {
+    forgot.classList.remove("activem");
+    document.querySelector(".forgotc").classList.add("hidden");
+  }
+};
+const f5 = function () {
+  forgot.classList.add("activem");
+  document.querySelector(".forgotc").classList.remove("hidden");
+  if (!login.classList.contains("hidden")) {
+    login.classList.remove("activem");
+    document.querySelector(".loginc").classList.add("hidden");
+  }
+  if (!register.classList.contains("hidden")) {
+    register.classList.remove("activem");
+    document.querySelector(".registerc").classList.add("hidden");
+  }
+};
 //Modal Window
 signin.addEventListener("click", f1);
 
 overlay.addEventListener("click", f2);
 
 closebtn.addEventListener("click", f2);
+
+register.addEventListener("click", f3);
+login.addEventListener("click", f4);
+forgot.addEventListener("click", f5);
