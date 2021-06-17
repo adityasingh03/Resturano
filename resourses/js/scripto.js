de82b357f2da6ddd0901ec9b257d81d3c34b9a47
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-06-17 12:28:26 +0530

    17/06

--- resourses/js/scripto.js
+++ resourses/js/scripto.js
@@ -8,15 +8,15 @@ let foodi = [
   "Idli",
   "Pizza",
   "Veg Rolls",
-  "Hot Dog",
-  "Butter Chicken",
-  "Chola",
+  "Veg Noodles",
+  "Chole",
   "Dal Fry",
   "Mix Veg Gravy",
-  "Chicken Muglai",
   "Palak Panner",
-  "Tikka Panner",
-  "Shrimps",
+  "Panner Tikka",
+  "Butter Chicken",
+  "Chicken Muglai",
+  "Biryani",
   "Thali",
   "Thali Pro",
   "Thali Pro Max",
@@ -24,7 +24,7 @@ let foodi = [
   "Roti",
   "Butter Roti",
   "Naan",
-  "Missi",
+  "Kulcha",
   "Coffee",
   "Tea",
   "Water",
@@ -35,40 +35,183 @@ let foodi = [
   "Ice Cream Stick",
 ];
 let costi = [
-  100, 60, 30, 120, 80, 250, 100, 60, 450, 150, 120, 140, 450, 180, 140, 440,
-  250, 450, 550, 350, 10, 15, 20, 20, 50, 40, 25, 30, 50, 60, 40, 40,
+  100, 60, 30, 120, 80, 250, 100, 80, 140, 120, 140, 160, 150, 400, 450, 200,
+  250, 400, 550, 350, 10, 15, 20, 25, 50, 30, 25, 45, 50, 60, 40, 40,
 ];
 let count = [];
-for (let i = 0; i < costi.length; i++) count.push(1);
+const f2 = function () {
+  cartin.innerHTML = "";
+  f1();
+};
+for (let i = 0; i < costi.length; i++) count.push(0);
 const addc = document.querySelectorAll(".btn");
 addc[0].addEventListener("click", function () {
   count[0]++;
-  f1();
+  f2();
 });
 
 addc[1].addEventListener("click", function () {
   count[1]++;
-  f1();
+  f2();
 });
 
 addc[2].addEventListener("click", function () {
   count[2]++;
-  f1();
 });
 
 addc[3].addEventListener("click", function () {
   count[3]++;
+  cartin.innerHTML = "";
   f1();
-  console.log(count);
 });
+
+addc[4].addEventListener("click", function () {
+  count[4]++;
+  f2();
+});
+
+addc[5].addEventListener("click", function () {
+  count[5]++;
+  f2();
+});
+
+addc[6].addEventListener("click", function () {
+  count[6]++;
+  f2();
+});
+
+addc[7].addEventListener("click", function () {
+  count[7]++;
+  f2();
+});
+
+addc[8].addEventListener("click", function () {
+  count[8]++;
+  f2();
+});
+
+addc[9].addEventListener("click", function () {
+  count[9]++;
+  f2();
+});
+
+addc[10].addEventListener("click", function () {
+  count[10]++;
+  f2();
+});
+
+addc[11].addEventListener("click", function () {
+  count[11]++;
+  f2();
+});
+
+addc[12].addEventListener("click", function () {
+  count[12]++;
+  f2();
+});
+
+addc[13].addEventListener("click", function () {
+  count[13]++;
+  f2();
+});
+
+addc[14].addEventListener("click", function () {
+  count[14]++;
+  f2();
+});
+
+addc[15].addEventListener("click", function () {
+  count[15]++;
+  f2();
+});
+
+addc[16].addEventListener("click", function () {
+  count[16]++;
+  f2();
+});
+
+addc[17].addEventListener("click", function () {
+  count[17]++;
+  f2();
+});
+
+addc[18].addEventListener("click", function () {
+  count[18]++;
+  f2();
+});
+
+addc[19].addEventListener("click", function () {
+  count[19]++;
+  f2();
+});
+
+addc[20].addEventListener("click", function () {
+  count[20]++;
+  f2();
+});
+
+addc[21].addEventListener("click", function () {
+  count[21]++;
+  f2();
+});
+
+addc[22].addEventListener("click", function () {
+  count[22]++;
+  f2();
+});
+
+addc[23].addEventListener("click", function () {
+  count[23]++;
+  f2();
+});
+
+addc[24].addEventListener("click", function () {
+  count[24]++;
+  f2();
+});
+
+addc[25].addEventListener("click", function () {
+  count[25]++;
+  f2();
+});
+
+addc[26].addEventListener("click", function () {
+  count[26]++;
+  f2();
+});
+
+addc[27].addEventListener("click", function () {
+  count[27]++;
+  f2();
+});
+
+addc[28].addEventListener("click", function () {
+  count[28]++;
+  f2();
+});
+
+addc[29].addEventListener("click", function () {
+  count[29]++;
+  f2();
+});
+
+addc[30].addEventListener("click", function () {
+  count[30]++;
+  f2();
+});
+
+addc[31].addEventListener("click", function () {
+  count[31]++;
+  f2();
+});
+
 console.log(count);
 const cartin = document.querySelector(".carttext");
 const brk = document.querySelectorAll(".brk");
 const f1 = function () {
   brk.forEach(function (i1, j) {
-    blur.innerHTML = "";
     const html = ` <div class="item">
-  <img src="resourses/imgs/food/p1.png" class="imgfc" />
+  <img src="resourses/imgs/food/p${j}.png" class="imgfc" />
   <p> ${foodi[j]} </p>
   <p>Rs ${costi[j]}</p>
   <div>
